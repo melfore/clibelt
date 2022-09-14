ddaca13bf7414637a9a898d56d09708b9b01317a
Alessandro Tilli <alessandro.tilli@digitalnology.com>
2022-09-14 16:55:33 +0200

    docs: add namespace sample files

---
+++ test/resources/ns4.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    code: "i18n.codes.defect.severity.AVERAGE",
+    tr: [
+      { l: "en", v: "Average" },
+      { l: "it", v: "Medio" },
+    ],
+  },
+  {
+    code: "i18n.codes.defect.severity.SEVERE",
+    tr: [
+      { l: "en", v: "Severe" },
+      { l: "it", v: "Grave" },
+    ],
+  },
+  {
+    code: "i18n.codes.defect.severity.SLIGHT",
+    tr: [
+      { l: "en", v: "Slight" },
+      { l: "it", v: "Lieve" },
+    ],
+  },
+  {
+    code: "i18n.codes.defect.types.OTHERS",
+    tr: [
+      { l: "en", v: "Others" },
+      { l: "it", v: "Altro" },
+    ],
+  },
+];
