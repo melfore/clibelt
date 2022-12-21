69f71a735b6f0bf7370e51a8ede8d28e08638c6e
Alessandro Tilli <alessandro.tilli@digitalnology.com>
2022-12-21 17:41:41 +0100

    chore: add override examples

--- test/resources/messages-all-langs.js
+++ test/resources/messages-all-langs.js
@@ -18,6 +18,34 @@ exports.messages = [
       { l: "en", v: "Sorry, the request is wrong (400)" },
       { l: "it", v: "Siamo spiacenti, la richiesta è errata (400)" },
     ],
+    overrides: [
+      {
+        customer: "c1",
+        tr: [
+          {
+            l: "en",
+            v: "Access denied C1",
+          },
+          {
+            l: "it",
+            v: "Accesso negato C1",
+          },
+        ],
+      },
+      {
+        customer: "c2",
+        tr: [
+          {
+            l: "en",
+            v: "Access denied C2",
+          },
+          {
+            l: "it",
+            v: "Accesso negato C2",
+          },
+        ],
+      },
+    ],
   },
 
   {
